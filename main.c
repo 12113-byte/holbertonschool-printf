f62556a9b0b243aa4513559b662385072da28bf4
Joleen <12113@holbertonstudents.com>
2025-11-17 03:33:54 +0000

    main file

---
+++ main.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <limit.h>
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: parameter to be checked for size.
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		length++;
+	}
+	return (length);
+}
+
+/**
+ *
+ *
+ */
+
+int _printf(const char *format, ...)
+{
+va_list args;
+va_start(args, format);
+
+while (*format)
+{
+	if (*format == '%')
+	{
+		format++;
+		if (*format == 's')
+		{
+			char *s = va_arg(args, char *);
+		}
+		else if (*format == 'c')
+		{
+			char c = (char)va_arg(args, int);
+		}
+		else
+			print char literally
+	}
+}
+
+
+
