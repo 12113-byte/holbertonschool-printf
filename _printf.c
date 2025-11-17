4b787951c4cec9bda3589ac93fed4f1eb73e53ea
Joleen <12113@holbertonstudents.com>
2025-11-17 06:18:58 +0000

    _printf file

---
+++ _printf.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - prints all arguments
+ *
+ * Return: counter
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int counter = 0;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '%')
+			{
+				write(1, "%", 1);
+				counter++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				if (str == NULL)
+				{
+					return (0);
+				}
+				while (*str)
+				{
+					write(1, str, 1);
+					str++;
+					counter++;
+				}
+			}
+			else if (*format == 'c')
+			{
+				int c = (char)va_arg(args, int);
+				write(1, &c, 1);
+				counter++;
+			}
+			else
+			{
+				write(1, format, 1);
+				counter++;
+			}
+
+			format++;
+		}
+		else
+		{
+			write(1, format, 1);
+			counter++;
+		}
+	}
+	va_end(args);
+	return (counter);
+}
+
