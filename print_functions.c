b7e0c5c0f786059db89ef4e5a060c06482c9dff7
Joleen <12113@holbertonstudents.com>
2025-11-18 02:21:00 +0000

    print functions

--- print_functions.c
+++ print_functions.c
@@ -5,35 +5,40 @@
 /**
  * print_a_char - prints  only  a char
  */
-void print_a_char(va_list *args)
+int print_a_char(va_list *args)
 {
 	char c = va_arg(*args, int);
 	write(1, &c, 1);
+    return 1;
 }
 
 /**
  * print_a_char_ptr - prints a char pointer
  */
-void print_a_str(va_list *args)
+int print_a_str(va_list *args)
 {
 	char *s = va_arg(*args, char*);
 	if (s == NULL)
     {
         s = "(null)";
     }
+    int len = 0;
     while (*s)
 	{
         write(1, s, 1);
         s++;
+        len++;
     }
+    return (len);
 }
 
 /**
 * print_special - prints a literal %
 */
-void print_special(va_list *args)
+int print_special(va_list *args)
 {
     (void)args;
     write(1, "%", 1);
+    return 1;
 }
 
