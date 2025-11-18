6ea100ae7a5ad0a07b038b7695eb01382a318377
MattW-12106 <12106@holbertonstudents.com>
2025-11-18 01:17:07 +0000

    printy

--- _printf.c
+++ _printf.c
@@ -9,49 +9,46 @@
  * Return: counter
  */
 
+typedef struct specifier {
+    char specifier;
+    void (*print_func)(va_list *);
+} specifier_t;
+
+specifier_t handlers[] = {
+    { 'c', print_a_char },
+    { 's', print_a_str },
+    { '%', print_special },
+    { 0, NULL }
+};
+
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int counter = 0;
+	int counter;
 
 	va_start(args, format);
 
-	while (*format == specifier_t)
+	while (*format)
 	{
-		if (handlers[counter].specifier == format[str])
+		if (*format == '%')
 		{
-			handlers[counter].print_func(&args)
-			}
-			else if (*format == 's')
+			char next = (*format + 1);
+			int found = 0;
+			for (counter = 0; handlers[counter].specifier != 0; counter++)
 			{
-				char *str = va_arg(args, char*);
-				if (str == NULL)
+				if (handlers[counter].specifier == next)
 				{
-					write(1, "(null)", 6);
-					counter += 6;
-				}
-				else
-				{
-				while (*str)
-				{
-					write(1, str, 1);
-					str++;
-					counter++;
-				}
+					handlers[counter].print_func(&args);
+					found = 1;
+					break;
 				}
 			}
-			else if (*format == 'c')
-			{
-				int c = (char)va_arg(args, int);
-				write(1, &c, 1);
-				counter++;
-			}
-			else
-			{
-				write(1, format, 1);
-				counter++;
-			}
+		if (found != 0)
+		{
+			write(1, "%", 1);
+			write(1, &next, 1);
 		}
+		format += 2;
 		else
 		{
 			write(1, format, 1);
