e389eb1c0e28265ba49ea6a1cbb6fb022c8be765
Joleen <12113@holbertonstudents.com>
2025-11-18 02:20:48 +0000

    header file

--- main.h
+++ main.h
@@ -4,13 +4,13 @@
 
 int _printf(const char *format, ...);
 int _strlen(char *s);
-void print_a_str(va_list *args);
-void print_special(va_list *args);
-void print_a_char(va_list *args);
+int print_a_str(va_list *args);
+int print_special(va_list *args);
+int print_a_char(va_list *args);
 
 typedef struct specifier {
     char specifier;
-    void (*print_func)(va_list *);
+    int (*print_func)(va_list *);
 } specifier_t;
 
 #endif
