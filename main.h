0d1211a3dc17b274b051031a8ab05e4dee10346d
MattW-12106 <12106@holbertonstudents.com>
2025-11-19 23:40:26 +0000

    main.h

--- main.h
+++ main.h
@@ -9,6 +9,10 @@ int print_special(va_list *args);
 int print_a_char(va_list *args);
 int print_an_int_or_decimal(va_list *args);
 int print_binary(va_list *args);
+int print_u(va_list *args)
+int print_o(va_list *args)
+int print_x(va_list *args)
+int print_X(va_list *args)
 
 typedef struct specifier
 {
