fd68447039aaaf1a738cccea41e7f480bfe4baf4
MattW-12106 <12106@holbertonstudents.com>
2025-11-21 00:10:28 +0000

    help

--- main.h
+++ main.h
@@ -9,12 +9,12 @@ int print_special(va_list *args);
 int print_a_char(va_list *args);
 int print_an_int_or_decimal(va_list *args);
 int print_binary(va_list *args);
-int print_unsigned_base(va_list *args, unsigned int base, int uppercase);
+int print_unsigned_base(va_list args, unsigned int base, int uppercase);
 void print_unsigned_recursive(unsigned int n, unsigned int base, int uppercase);
-int print_unsigned_u(va_list *args);
-int print_unsigned_o(va_list *args);
-int print_unsigned_x(va_list *args);
-int print_unsigned_X(va_list *args);
+int print_unsigned_u(va_list args);
+int print_unsigned_o(va_list args);
+int print_unsigned_x(va_list args);
+int print_unsigned_X(va_list args);
 
 typedef struct specifier
 {
